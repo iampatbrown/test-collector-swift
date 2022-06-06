97f44449b63f2de35d072b67ec6d3d3ec9f9cad9
Pat Brown <mrpatbrown@gmail.com>
2022-06-06 17:27:57 +1000

    Bumped windows tests

--- Tests/CoreTests/ApiClientTests.swift
+++ Tests/CoreTests/ApiClientTests.swift
@@ -6,21 +6,15 @@ import FoundationNetworking
 #endif
 
 final class ApiClientTests: XCTestCase {
-  func testApiClientMakesUploadRequest() throws {
+  func testApiClientMakesUploadRequest() async throws {
     struct Response: Decodable { var status: String }
     let server = MockServer(responses: .json("{\"status\":\"OK\"}"))
     let api = ApiClient.live(apiToken: "token", session: server.session)
     let results = TestResults.json(runEnv: .init(key: "key"), data: [])
 
-    let expectation = self.expectation(description: "response")
-    Task {
-      let (value, _) = try await api.data(for: .upload(results), as: Response.self)
-      XCTAssertEqual(value.status, "OK")
-      expectation.fulfill()
-    }
-    
-    self.wait(for: [expectation], timeout: 0.1)
-  
+    let (value, _) = try await api.data(for: .upload(results), as: Response.self)
+
+    XCTAssertEqual(value.status, "OK")
     server.received { request in
       XCTAssertEqual("https://analytics-api.buildkite.com/v1/upload", request.url?.absoluteString)
       XCTAssertEqual("Token token=\"token\"", request.authorizationHeader)
