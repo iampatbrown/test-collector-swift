8e71f3ff542a6b11d0d5ce891d4ed63b4a595708
Pat Brown <mrpatbrown@gmail.com>
2022-06-06 16:41:15 +1000

    wip

--- Tests/CoreTests/TestHelpers/ApiClient.swift
+++ Tests/CoreTests/TestHelpers/ApiClient.swift
@@ -10,7 +10,7 @@ extension ApiClient {
   static func fulfill(_ expectation: XCTestExpectation, after seconds: TimeInterval) -> ApiClient {
     ApiClient { _ in
       try await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
-      DispatchQueue.main.async { expectation.fulfill() }
+      expectation.fulfill()
       await Task.yield()
       return (Data(), HTTPURLResponse.success())
     }
