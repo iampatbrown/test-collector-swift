3f18118378c61facef3ac09e264fd7aec676c59e
Pat Brown <mrpatbrown@gmail.com>
2022-06-07 10:46:56 +1000

    Also won't fail

--- Examples/DemoLibrary/Tests/DemoLibraryTests/FlackyTests.swift
+++ Examples/DemoLibrary/Tests/DemoLibraryTests/FlackyTests.swift
@@ -2,7 +2,7 @@
 import XCTest
 
 final class FlackyTests: XCTestCase {
-  let allTestsShouldPass = Double.random(in: 0...1) < 4
+  let allTestsShouldPass = Double.random(in: 0...1) < 5
 
   func shouldFail() -> Bool {
     !self.allTestsShouldPass && Bool.random()
