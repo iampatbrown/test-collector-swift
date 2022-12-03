706f2e92a979c3c91c5198bbfb3941fb9bae1c4f
Pat Brown <mrpatbrown@gmail.com>
2022-12-03 12:53:33 +1100

    Remove benchmark test

--- Examples/DemoApp/DemoAppUITests/DemoAppUITests.swift
+++ Examples/DemoApp/DemoAppUITests/DemoAppUITests.swift
@@ -11,12 +11,4 @@ final class DemoAppUITests: XCTestCase {
     let app = XCUIApplication()
     app.launch()
   }
-
-  func testLaunchPerformance() throws {
-    if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-      measure(metrics: [XCTApplicationLaunchMetric()]) {
-        XCUIApplication().launch()
-      }
-    }
-  }
 }
