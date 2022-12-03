356b807166b493293e55816bfe087211607760e4
Pat Brown <mrpatbrown@gmail.com>
2022-12-03 13:39:15 +1100

    Mark loadCollector with @_spi for release

--- Sources/BuildkiteTestCollector/Loader.swift
+++ Sources/BuildkiteTestCollector/Loader.swift
@@ -1,7 +1,8 @@
 import Core
 
 /// This function is automatically called by the Loader module to ensure the test collector is loaded before running any tests
+@_spi(Loader)
 @_cdecl("loadCollector")
-func loadCollector() {
+public func loadCollector() {
   Core.TestCollector.load()
 }
