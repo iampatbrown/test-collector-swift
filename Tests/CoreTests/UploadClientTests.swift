ad56fe6c4c411274b8158ea179b23f8eb962c30c
Pat Brown <mrpatbrown@gmail.com>
2022-12-11 17:32:05 +1100

    Remove sleep from tests

--- Tests/CoreTests/UploadClientTests.swift
+++ Tests/CoreTests/UploadClientTests.swift
@@ -11,8 +11,7 @@ final class UploadClientTests: XCTestCase {
     let uploadClient = UploadClient.live(api: .fulfill(uploadCompleted, after: 0.5), runEnvironment: EnvironmentValues().runEnvironment())
     let trace = Trace(id: "id", history: .init(section: "section"))
 
-    Task { try await uploadClient.upload(trace: trace) }
-    self.sleep(for: 0.01)
+    Task(priority: .background) { try await uploadClient.upload(trace: trace) }
     uploadClient.waitForUploads()
 
     self.wait(for: [uploadCompleted], timeout: 0)
@@ -24,8 +23,7 @@ final class UploadClientTests: XCTestCase {
     let uploadClient = UploadClient.live(api: .fulfill(uploadCompleted, after: 0.5), runEnvironment: EnvironmentValues().runEnvironment())
     let trace = Trace(id: "id", history: .init(section: "section"))
 
-    let task = Task { try await uploadClient.upload(trace: trace) }
-    self.sleep(for: 0.01)
+    let task = Task(priority: .background) { try await uploadClient.upload(trace: trace) }
     uploadClient.waitForUploads(timeout: 0.1)
     task.cancel()
 
