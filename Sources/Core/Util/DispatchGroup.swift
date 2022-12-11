321f6b7a38a56fe0e8a1eebde8f9c609cb8975f6
Pat Brown <mrpatbrown@gmail.com>
2022-12-12 10:47:17 +1100

    Update DispatchGroup.swift

--- Sources/Core/Util/DispatchGroup.swift
+++ Sources/Core/Util/DispatchGroup.swift
@@ -6,11 +6,10 @@ extension DispatchGroup {
   /// - Parameter timeout: The maximum duration in seconds to wait.
   /// - Returns: A result value indicating whether the method returned due to a timeout.
   func yieldAndWait(timeout: TimeInterval) -> DispatchTimeoutResult {
-    let semaphore = DispatchSemaphore(value: 0)
+    self.enter()
     DispatchQueue.global(qos: .background).async {
-      semaphore.signal()
+      self.leave()
     }
-    semaphore.wait()
     return self.wait(timeout: .now() + .milliseconds(Int(timeout * 1000)))
   }
 }
