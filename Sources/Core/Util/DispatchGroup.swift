c9cfae848d87d9333a8939bf0a0192dee0235580
Pat Brown <mrpatbrown@gmail.com>
2022-12-12 13:52:26 +1100

    Update DispatchGroup.swift

--- Sources/Core/Util/DispatchGroup.swift
+++ Sources/Core/Util/DispatchGroup.swift
@@ -6,13 +6,15 @@ extension DispatchGroup {
   /// - Parameter timeout: The maximum duration in seconds to wait.
   /// - Returns: A result value indicating whether the method returned due to a timeout.
   func yieldAndWait(timeout: TimeInterval) -> DispatchTimeoutResult {
-    self.enter()
-    Task(priority: .background) {
-      for _ in 1...10 {
-        await Task.detached(priority: .background) { await Task.yield() }.value
+    for _ in 0..<10 {
+      self.enter()
+      Task.detached(priority: .background) {
+        try? await Task.sleep(nanoseconds: 1)
+        self.leave()
       }
-      self.leave()
+      _ = self.wait(timeout: .now() + .nanoseconds(1))
     }
+
     return self.wait(timeout: .now() + .milliseconds(Int(timeout * 1000)))
   }
 }
