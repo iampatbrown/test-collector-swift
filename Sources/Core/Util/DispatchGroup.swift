61fe156f4c9b988e8e59b5b6ab79701afab1a48d
Pat Brown <mrpatbrown@gmail.com>
2022-12-12 14:13:07 +1100

    Simplify yield

--- Sources/Core/Util/DispatchGroup.swift
+++ Sources/Core/Util/DispatchGroup.swift
@@ -6,15 +6,10 @@ extension DispatchGroup {
   /// - Parameter timeout: The maximum duration in seconds to wait.
   /// - Returns: A result value indicating whether the method returned due to a timeout.
   func yieldAndWait(timeout: TimeInterval) -> DispatchTimeoutResult {
+    let semaphore = DispatchSemaphore(value: 0)
     for _ in 0..<10 {
-      self.enter()
-      Task.detached(priority: .background) {
-        try? await Task.sleep(nanoseconds: 1)
-        self.leave()
-      }
-      _ = self.wait(timeout: .now() + .nanoseconds(1))
+      _ = semaphore.wait(timeout: .now() + .nanoseconds(1))
     }
-
     return self.wait(timeout: .now() + .milliseconds(Int(timeout * 1000)))
   }
 }
